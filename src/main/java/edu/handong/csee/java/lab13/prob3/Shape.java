866a96f5c323e4d071d44fd576906cd37efe70b8
JoJeonghoon <akima95@naver.com>
2018-05-03 20:58:16 +0900

    Add single line comments for Shape abstract class

--- src/main/java/edu/handong/csee/java/lab13/prob3/Shape.java
+++ src/main/java/edu/handong/csee/java/lab13/prob3/Shape.java
@@ -1,14 +1,14 @@
-package edu.handong.csee.java.lab13.prob3;
+package edu.handong.csee.java.lab13.prob3;  //package name
 
-public abstract class Shape {
+public abstract class Shape { //Shape is abstract class
 
-	public abstract double getArea();
+	public abstract double getArea();  //it has getArea abstract method
 	
-	public abstract double getPerimeter();
+	public abstract double getPerimeter(); //it has getPerimeter abstract method
 	
-	public void show() {
-		System.out.println("Area: " + getArea());
-		System.out.println("Perimeter: " + getPerimeter());
+	public void show() { //showing result
+		System.out.println("Area: " + getArea());  //print area value
+		System.out.println("Perimeter: " + getPerimeter()); //print perimeter value
 	}
 
 }
