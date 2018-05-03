0a7e74fe6a3f9c8914e629bfdec364e2a1d56d5a
JoJeonghoon <akima95@naver.com>
2018-05-03 20:53:51 +0900

    Add single line comments for Circle class

--- src/main/java/edu/handong/csee/java/lab13/prob3/Circle.java
+++ src/main/java/edu/handong/csee/java/lab13/prob3/Circle.java
@@ -1,24 +1,24 @@
-package edu.handong.csee.java.lab13.prob3;
+package edu.handong.csee.java.lab13.prob3;  //package name
 
-public class Circle extends Shape{
+public class Circle extends Shape{ //Circle class inherits Shape class, which is abstract class.
 
-	private double radius;
+	private double radius; //it has radius data
 	
-	public Circle(double radius) {
-		this.radius = radius;
+	public Circle(double radius) { //constructor
+		this.radius = radius; //set initial radius value for instance
 	}
 	
 	@Override
-	public double getArea() {
+	public double getArea() { //getting area, and it is abstract method
 		
-		return Math.PI * radius * radius;
+		return Math.PI * radius * radius; //return area value
 		
 	}
 
 	@Override
-	public double getPerimeter() {
+	public double getPerimeter() { //getting perimeter, and it is also abstract method
 		
-		return 2* Math.PI * radius;
+		return 2* Math.PI * radius; //returns perimeter value
 	}
 	
 	
