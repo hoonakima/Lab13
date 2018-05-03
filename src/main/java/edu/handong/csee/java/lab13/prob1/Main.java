526566e45e345e5e1aca6fcc09f330a3729be898
JoJeonghoon <akima95@naver.com>
2018-05-03 17:22:21 +0900

    Add line comments for Main class

--- src/main/java/edu/handong/csee/java/lab13/prob1/Main.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Main.java
@@ -1,18 +1,17 @@
-package edu.handong.csee.java.lab13.prob1;
+package edu.handong.csee.java.lab13.prob1;  //package name
 
-public class Main {
+public class Main {  //Main class
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { //main method to start this program
 
-		Friend friend1 = new Friend("Jeong hoon Jo"); //Just Friend
-		Friend friend2 = new SchoolFriend("Jin su Jo"); //School Friend
-		Friend friend3 = new ClassFriend("Na kyung Lee"); //Class Friend
+		Friend friend1 = new Friend("Jeong hoon Jo"); //Create Friend instance
+		Friend friend2 = new SchoolFriend("Jin su Jo"); //Create SchoolFriend instance
+		Friend friend3 = new ClassFriend("Na kyung Lee"); //Create ClassFriend instance
 
-		//When each class has different method, then we need to cast it to call the right method for each object.
-		//That's why we use instanceOf.
-		InstanceOf.whatFriend(friend1);
-		InstanceOf.whatFriend(friend2);
-		InstanceOf.whatFriend(friend3);
+		//we don't need to instantiate the InstanceOf class, because whatFriend() is static method. 
+		InstanceOf.whatFriend(friend1); //call whatFriend method with friend1, which is Friend
+		InstanceOf.whatFriend(friend2); //call whatFriend method with friend2, which is SchoolFriend
+		InstanceOf.whatFriend(friend3); //call whatFriend method with friend3, which is ClassFriend
 
 	}		
 }
