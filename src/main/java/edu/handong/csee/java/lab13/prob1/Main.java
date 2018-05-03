f2c8ef0888183e6f6fa7718168558d8a8723b604
JoJeonghoon <akima95@naver.com>
2018-05-03 16:11:40 +0900

    Add a main method

--- src/main/java/edu/handong/csee/java/lab13/prob1/Main.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/Main.java
@@ -3,8 +3,16 @@ package edu.handong.csee.java.lab13.prob1;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+		Friend[] friend = new Friend[3];
 
+		friend[0] = new Friend("Jeong hoon Jo"); //Just Friend
+		friend[1] = new SchoolFriend("Jin su Jo"); //School Friend
+		friend[2] = new ClassFriend("Na kyung Lee"); //Class Friend
+
+		InstanceOf.whatFriend(friend[0]);
+		InstanceOf.whatFriend(friend[1]);
+		InstanceOf.whatFriend(friend[2]);
+
+	}		
 }
