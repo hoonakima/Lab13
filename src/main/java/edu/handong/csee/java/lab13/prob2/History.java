3956935974f6d8f15037fba84776c8dfbca09ff2
JoJeonghoon <akima95@naver.com>
2018-05-03 19:51:04 +0900

    Add line comments for History class

--- src/main/java/edu/handong/csee/java/lab13/prob2/History.java
+++ src/main/java/edu/handong/csee/java/lab13/prob2/History.java
@@ -1,33 +1,33 @@
-package edu.handong.csee.java.lab13.prob2;
+package edu.handong.csee.java.lab13.prob2; //package name
 
-public class History extends Book{
+public class History extends Book{ //History class inherits Book class.
 	
-	private String author;
+	private String author; //it has author data, too.
 	
-	public History() {
-		super();
-		author = "No author name yet";
+	public History() { //default constructor
+		super(); //call base class constructor
+		author = "No author name yet"; //set default author data 
 		
 	}
 	
-	public History(String bookName, String author) {
-		super(bookName);
-		this.author = author;
+	public History(String bookName, String author) { //constructor
+		super(bookName); //call base class constructor
+		this.author = author; //set author name as input data
 	}
 
-	public String getAuthor() {
-		return author;
+	public String getAuthor() { //getting author name method
+		return author; //returns author name
 	}
 
-	public void setAuthor(String author) {
-		this.author = author;
+	public void setAuthor(String author) { //setting author name
+		this.author = author; //set author name as input data
 	}
 	
-	public void showBookInfo(int i) {
-		System.out.println(toString());
-		System.out.println("\t Id: " + i);
-		System.out.println("\t Book name: " + getBookName());
-		System.out.println("\t Author: " + author);
+	public void showBookInfo(int i) { //showing book information
+		System.out.println(toString()); //print class name
+		System.out.println("\t Id: " + i); //print Id number of a book
+		System.out.println("\t Book name: " + getBookName()); //print book name
+		System.out.println("\t Author: " + author); //print author name
 	}
 
 }
