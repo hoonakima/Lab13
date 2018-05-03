300a317df1c5621876a7481ce05fac3b805337f2
JoJeonghoon <akima95@naver.com>
2018-05-03 17:18:27 +0900

    Add line comments for InstanceOf class

--- src/main/java/edu/handong/csee/java/lab13/prob1/InstanceOf.java
+++ src/main/java/edu/handong/csee/java/lab13/prob1/InstanceOf.java
@@ -1,19 +1,21 @@
-package edu.handong.csee.java.lab13.prob1;
+package edu.handong.csee.java.lab13.prob1; //package name
 
-public class InstanceOf {
-
-	static public void whatFriend(Friend friend) {
-
-		if(friend instanceof ClassFriend) {
-			((ClassFriend)friend).classFriend();
+public class InstanceOf { //InstanceOf class
+						  //This class is for calling the different named method depending on object.
+						  //If each object has same named method, we don't need to use instanceof, because of Dynamic Binding.
+	
+	static public void whatFriend(Friend friend) { //define whatFriend static method 
+												   
+		if(friend instanceof ClassFriend) { //check a type of objects is ClassFriend
+			((ClassFriend)friend).classFriend(); //type casting to ClassFriend and call classFriend method
 		}
 
-		else if(friend instanceof SchoolFriend) {
-			((SchoolFriend)friend).schoolFriend();
+		else if(friend instanceof SchoolFriend) { //check a type of objects is SchoolFriend
+			((SchoolFriend)friend).schoolFriend(); //type casting to SchoolFriend and call schoolFriend method
 		}
 
-		else
-			friend.justFriend();
+		else //when the type of object is Friend
+			friend.justFriend(); // call justFriend method in Friend class.
 
 	}
 
