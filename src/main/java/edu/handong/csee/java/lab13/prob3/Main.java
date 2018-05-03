a0f53881379dd76adc73208eed5080ad5a2eb2b8
JoJeonghoon <akima95@naver.com>
2018-05-03 21:02:16 +0900

    Add single line comments for Main class

--- src/main/java/edu/handong/csee/java/lab13/prob3/Main.java
+++ src/main/java/edu/handong/csee/java/lab13/prob3/Main.java
@@ -1,27 +1,27 @@
-package edu.handong.csee.java.lab13.prob3;
+package edu.handong.csee.java.lab13.prob3; //package name
 
-import java.util.Scanner;
+import java.util.Scanner; //import Scanner class
 
-public class Main {
+public class Main { //Main class	
+		
+	public static void main(String[] args) { //starting this program
 
-	public static void main(String[] args) {
-
-		Scanner keyboard = new Scanner(System.in);
+		Scanner keyboard = new Scanner(System.in); //instantiate Scanner object
 		
 		
-		System.out.print("Enter radius: ");
-		double radius = keyboard.nextDouble();
-		Circle circle = new Circle(radius);
-		System.out.println("Radius: " + radius);
-		circle.show();
+		System.out.print("Enter radius: "); //print the line
+		double radius = keyboard.nextDouble(); //get radius value from keyboard
+		Circle circle = new Circle(radius); //instantiate Circle object
+		System.out.println("Radius: " + radius); //print radius value
+		circle.show(); //show the results for circle instance
 		
-		System.out.print("\nEnter length and width: ");
-		double length = keyboard.nextDouble();
-		double width = keyboard.nextDouble();
-		Rectangle rectangle = new Rectangle(length, width);
-		System.out.println("Length: " + length);
-		System.out.println("Width: " + width);
-		rectangle.show();
+		System.out.print("\nEnter length and width: "); //print the line
+		double length = keyboard.nextDouble(); //get length value from keyboard
+		double width = keyboard.nextDouble(); //get width value from keyboard
+		Rectangle rectangle = new Rectangle(length, width); //instantiate Rectangle object
+		System.out.println("Length: " + length); //print length value
+		System.out.println("Width: " + width); //print width value
+		rectangle.show(); //show the results for rectangle instance
 		
 		
 	}
