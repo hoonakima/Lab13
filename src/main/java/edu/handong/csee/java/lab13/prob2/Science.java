bfc458e10f2f59e6309c84dca122a6b49e5f8c8b
JoJeonghoon <akima95@naver.com>
2018-05-03 19:47:54 +0900

    Add line comments for Science class

--- src/main/java/edu/handong/csee/java/lab13/prob2/Science.java
+++ src/main/java/edu/handong/csee/java/lab13/prob2/Science.java
@@ -1,32 +1,32 @@
-package edu.handong.csee.java.lab13.prob2;
+package edu.handong.csee.java.lab13.prob2; //package name
 
-public class Science extends Book{
+public class Science extends Book{ //Science class inherits Book class
 	
-	private String publisher;
+	private String publisher; //it has publisher data, too
 	
-	public Science() {
-		super();
-		publisher = "No publisher yet";
+	public Science() { //default constructor
+		super();  //call the constructor of base class
+		publisher = "No publisher yet"; //set publisher data like that.
 	}
 	
-	public Science(String bookName, String publisher) {
-		super(bookName);
-		this.publisher = publisher;
+	public Science(String bookName, String publisher) { //constructor
+		super(bookName); //call the constructor of base class
+		this.publisher = publisher; //set publisher as input data
 	}
 
-	public String getPublisher() {
-		return publisher;
+	public String getPublisher() { //getting publisher name method
+		return publisher; //returns publisher name
 	}
 
-	public void setPublisher(String publisher) {
-		this.publisher = publisher;
+	public void setPublisher(String publisher) { //setting publisher name method
+		this.publisher = publisher; //set publisher name as input data
 	}
 
-	public void showBookInfo(int i) {
-		System.out.println(toString());
-		System.out.println("\t Id: " + i);
-		System.out.println("\t Book Name: " + getBookName()); 
-		System.out.println("\t Publisher: " + publisher);
+	public void showBookInfo(int i) { //showing book information method. Input i is for counting number of books.
+		System.out.println(toString()); //inherited method. It shows class name
+		System.out.println("\t Id: " + i); //print Id number of a book
+		System.out.println("\t Book Name: " + getBookName()); //print book name
+		System.out.println("\t Publisher: " + publisher); //print publisher name
 		
 	}
 	
