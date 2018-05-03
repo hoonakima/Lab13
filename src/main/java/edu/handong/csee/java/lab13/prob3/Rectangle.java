c028d97946944b26b875298c704eefffe7bc53f0
JoJeonghoon <akima95@naver.com>
2018-05-03 20:56:31 +0900

    Add single line comments for Rectangle class

--- src/main/java/edu/handong/csee/java/lab13/prob3/Rectangle.java
+++ src/main/java/edu/handong/csee/java/lab13/prob3/Rectangle.java
@@ -1,23 +1,23 @@
-package edu.handong.csee.java.lab13.prob3;
+package edu.handong.csee.java.lab13.prob3;  //package name
 
-public class Rectangle extends Shape{
+public class Rectangle extends Shape{ //Rectangle class inherits Shape class which is abstract class.
 
-	private double length;
-	private double width;
+	private double length; //it has length data
+	private double width;  //it has width data
 	
-	public Rectangle(double length, double width) {
-		this.length = length;
-		this.width = width;
+	public Rectangle(double length, double width) { //constructor
+		this.length = length; //set initial length value for an instance
+		this.width = width;  //set initial width value for an instance
 	}
 	
 	@Override
-	public double getArea() {
-		return length*width;
+	public double getArea() { //abstract method getting area value
+		return length*width; //return area value
 	}
 
 	@Override
-	public double getPerimeter() {
-		return 2*(length + width);
+	public double getPerimeter() { //abstract method getting perimeter value
+		return 2*(length + width); //return perimeter value
 		
 	}
 
